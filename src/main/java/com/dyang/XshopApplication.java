e23cb4ec0cbe0da692785ed2469c65f0286d0808
yangjie <772847238@qq.com>
2018-07-02 17:37:08 +0800

    初始化

---
+++ src/main/java/com/dyang/XshopApplication.java
@@ -0,0 +1,12 @@
+package com.dyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XshopApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dyang/XshopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dyang;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XshopApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
