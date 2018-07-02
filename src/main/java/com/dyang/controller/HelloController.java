ea04976934f70b4f31b73060f52e2412a49ba66e
DaveYang <yaabbccj@163.com>
2018-07-02 21:56:14 +0800

    厨师花花

--- src/main/java/com/dyang/controller/HelloController.java
+++ src/main/java/com/dyang/controller/HelloController.java
@@ -1,13 +1,14 @@
 package com.dyang.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
-    private String index(){
-        return "Hello World!";
+    @RequestMapping("/login.html")
+    private String login(){
+        return "login";
     }
 }
