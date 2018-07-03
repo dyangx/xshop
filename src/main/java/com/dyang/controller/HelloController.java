3cdc4d30d5c14aa8ea7de4fb72a78ee8ea6c7691
yangjie <772847238@qq.com>
2018-07-03 17:09:29 +0800

    初始化

--- src/main/java/com/dyang/controller/HelloController.java
+++ src/main/java/com/dyang/controller/HelloController.java
@@ -2,13 +2,20 @@ package com.dyang.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/login.html")
-    private String login(){
-        return "login";
+    private ModelAndView login(){
+        ModelAndView view = new ModelAndView("login");
+        return view;
+    }
+
+    @RequestMapping("/login.do")
+    private String index(){
+        ModelAndView view = new ModelAndView("login.html");
+        return "hello,you!";
     }
 }
